59baf4c5d4bb3f7df779ae0c20bd8862585beb4d
David <davidgadyan92@gmail.com>
2023-05-17 14:09:59 +0200

    SC faucet update

--- Smart_Contracts/faucet/contracts/FaucetContract.sol
+++ Smart_Contracts/faucet/contracts/FaucetContract.sol
@@ -3,6 +3,39 @@
 
  contract Faucet {
      // storage variables
-     uint public funds = 1000;
-     int public counter = -10; 
- }
+    //  uint public funds = 1000;
+
+    // special function
+    // called when make tx that does not specify function
+    // external are parts of contract interface
+    // can be called by other contracts and transactions
+
+    // create address
+    // address[] public funders;
+    uint public numOfFunder;
+
+    mapping (uint => address) public funders;
+
+
+    // payable can provide value
+    receive() external payable {
+    }
+
+    function addFunds () external payable {
+        // funders.push(msg.sender);
+        uint index = numOfFunder++;
+        funders[index] = msg.sender;
+    }
+
+    // function getAllFunders() external view returns(address[] memory){
+    //     return funders;
+    // }
+
+    function getFunderAtIndex (uint index) external view returns(address){
+        return funders[index];
+    }
+}
+
+ // Nonce a hash when combined with minHash proofs that
+ // block has gine through proof of work(POW)
+ // 8 bytes 64 bits
